9eaf1df910fa2ac378db7caa78716dfbbb8a3bdd
Jbrandstetter <86755371+Rahanoth95@users.noreply.github.com>
2023-04-07 17:10:46 -0700

    Add files via upload

---
+++ Project_2_Skeleton/division.h
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+class Division: public SubExpression {
+public:
+    Division(Expression* left, Expression* right): SubExpression(left, right) {
+    }
+    double evaluate() {
+       return left->evaluate() / right->evaluate();
+    }
+};
---
+++ Project_2_Skeleton/multiplication.h
@@ -0,0 +1,9 @@
+
+class Multiplication: public SubExpression {
+public:
+    Multiplication(Expression* left, Expression* right): SubExpression(left, right) {
+    }
+    double evaluate() {
+       return left->evaluate() * right->evaluate();
+    }
+};
